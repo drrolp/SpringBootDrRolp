8b204cd7610b33804eda779b89c6df9ab8e73b15
ethien-salinas-movile <ethien.salinas@movile.com>
2015-05-02 11:51:28 -0500

    ADD Comments

--- README.md
+++ README.md
@@ -8,9 +8,14 @@ Proyecto que ejemplifica el uso de SpringBoot usando las siguientes tecnologías
     * HSQLDB como motor
     * HikariCP para administrar el pool de conexiones
     * Hibernate 4 como ORM
+* Spring
+    * Spring Framework para la configuración del proyecto (IoC)
+    * Spring MVC para los servicios RESTful
+    * Spring Data para obtener métods que realizan operaciones básicas (CRUD) al manipular una entidad
 * Frontend
     * Thymeleaf 
     * Bootstrap
+    * jQuery
 
 ### Contribuidores
 * creado por: [DrRolp](https://github.com/drrolp)
--- src/main/java/com/auribox/springboot/model/Persona.java
+++ src/main/java/com/auribox/springboot/model/Persona.java
@@ -8,22 +8,29 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "persona")    // cambia en nombre por el de la tabla en la db
+/*
+* Por defecto el nombre de la tabla es el nombre de la entidad,
+* en caso de querer cambiarlo usamos la anotación @Table
+*/
+@Table(name = "persona")
 public class Persona {
 
-	@Id
-	@Column(name="id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)  // indica que es auntogenerada en la base de datos
+	@Id		// Toda entidad necesita tener su llave primaria que la identifique de manera única en la BD - http://docs.oracle.com/javaee/7/api/javax/persistence/Id.html
+	@GeneratedValue(strategy=GenerationType.IDENTITY)  // Definimos la estratégia para generar el ID - http://docs.oracle.com/javaee/7/api/javax/persistence/GenerationType.html
 	private int id;
-	
+
 	@Column(name="nombre")
 	private String nombre;
+	/*
+	* Por defecto el nombre de la columna es el mismo que del atributo,
+	* en caso de querer cambiarlo usamos la anotación @Column
+	* en este ejemplo el atributo "country" mapea a la columna "pais"
+	*/
 	@Column(name="pais")
 	private String country;
 	
-	public Persona() {
-		// TODO Auto-generated constructor stub
-	}
+	public Persona() {}
+
 	public String getNombre() {
 		return nombre;
 	}
