a55b6cb9520be272a1bc1ec1e873997d125584f2
ethien-salinas-movile <ethien.salinas@movile.com>
2015-05-02 01:03:30 -0500

    ADD gitignore file

--- src/main/java/com/auribox/springboot/dao/PersonaRepositoy.java
+++
@@ -1,9 +0,0 @@
-package com.auribox.springboot.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.auribox.springboot.model.Persona;
-
-public interface PersonaRepositoy extends CrudRepository<Persona, Integer> {
-
-}
